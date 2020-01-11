24203c90375ef0bc7ad5b9ef695b527e7b82a4d4
Ayush Jain <ayushjain20n@gmail.com>
2020-01-11 17:27:20 +0530

    Use discovery function to fetch service configuration

--- utils.py
+++ utils.py
@@ -1,22 +1,19 @@
-import os
-import yaml
+from django.conf import settings
 
 from categories.models.category import Category
 
 
 def get_base_category():
     """
+    This function returns the category node corresponding to this service
     :return: the Category object with slug field equal to service name
     """
 
-    config_path = os.path.join('services', 'helpcentre', 'config.yml')
-    with open(config_path, 'r') as file_object:
-        app_configuration = yaml.safe_load(file_object)
-    nomenclature = app_configuration['nomenclature']
-    service_name = nomenclature['name']
-    service_verbose = nomenclature['verboseName']
+    service_nomenclature = settings.DISCOVERY.get_app_configuration(
+        'helpcentre'
+    ).nomenclature
     cat_obj, created = Category.objects.get_or_create(
-        slug=service_name,
-        name=service_verbose
+        slug=service_nomenclature.name,
+        name=service_nomenclature.verboseName
     )
     return cat_obj
